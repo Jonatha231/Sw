dfe24bc31037990cd626137646266c525f398d04
Jonatha231 <161509156+Jonatha231@users.noreply.github.com>
2025-02-14 16:56:16 -0300

    Exemplos de Php

---
+++ aula02/exmplo01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo1_php</title>
+</head>
+<body>
+    <p> Oi sou o fulano</p>
+    <?php
+        echo "<P>Oi sou o fulano</p>";
+        $nome = "jow";
+        echo "<p> oi sou o $nome</p>";
+        echo "<p> oi sou o ".$nome."</p>";
+    ?>
+    
+</body>
+</html>
